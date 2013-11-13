5737184794a1557bc0e16f43bb5dcef80ec411f5
Michal Matyjek <michal.matyjek@gmail.com>
2013-11-12 18:03:15 -0800

    update

--- src/pl/com/oso/figury/MyRenderer.java
+++ src/pl/com/oso/figury/MyRenderer.java
@@ -12,6 +12,8 @@ public class MyRenderer implements Renderer {
 	public void onDrawFrame(GL10 gl) {
 		// TODO Auto-generated method stub
 		GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
+		Triangle t = new Triangle();
+		t.draw();
 
 	}
 
@@ -26,7 +28,28 @@ public class MyRenderer implements Renderer {
 	public void onSurfaceCreated(GL10 gl, EGLConfig config) {
 		// TODO Auto-generated method stub
 		GLES20.glClearColor(0.5f, 0.5f, 0.5f, 1.0f);
+		
+		// initialize a triangle
+	    Triangle mTriangle = new Triangle();
+	    // initialize a square
+	    Square mSquare = new Square();
 
 	}
+	
+	public static int loadShader(int type, String shaderCode){
+
+	    // create a vertex shader type (GLES20.GL_VERTEX_SHADER)
+	    // or a fragment shader type (GLES20.GL_FRAGMENT_SHADER)
+	    int shader = GLES20.glCreateShader(type);
+
+	    // add the source code to the shader and compile it
+	    GLES20.glShaderSource(shader, shaderCode);
+	    GLES20.glCompileShader(shader);
+
+	    return shader;
+	}
+	
+	
+	
 
 }
